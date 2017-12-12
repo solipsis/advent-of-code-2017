0295df7fa1d30f990b925ea4a967d8600fee6fc3
solipsis <david@keepkey.com>
2017-12-12 13:07:07 -0700

    challenge 8a

---
+++ 8a/8a.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+
+	file, _ := os.Open("input.txt")
+	sc := bufio.NewScanner(file)
+
+	// compute each operation
+	m := make(map[string]int)
+	for sc.Scan() {
+
+		arr := strings.Split(sc.Text(), " ")
+		reg, arg, amt, as, op, bs := arr[0], arr[1], arr[2], arr[4], arr[5], arr[6]
+		a := m[as]
+		b, _ := strconv.Atoi(bs)
+
+		// check if the condition is true and apply the effect
+		if cond(a, b, op) {
+			v, _ := strconv.Atoi(amt)
+			if arg == "inc" {
+				m[reg] += v
+			} else {
+				m[reg] -= v
+			}
+		}
+	}
+
+	max := 0
+	for _, v := range m {
+		if v > max {
+			max = v
+		}
+	}
+	fmt.Println(max)
+}
+
+// check if the given condition is true
+func cond(a, b int, op string) bool {
+	switch op {
+	case "<":
+		return a < b
+	case ">":
+		return a > b
+	case "<=":
+		return a <= b
+	case ">=":
+		return a >= b
+	case "!=":
+		return a != b
+	case "==":
+		return a == b
+	default:
+		return false
+	}
+}
